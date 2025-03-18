e0923cb3321cc13dfe8a3c17391e0941abb920a9
NaufalArkaan <naufalarkaan25@gmail.com>
2025-03-18 22:10:57 +0700

    income expense

--- incomedanexpense/Note.js
+++ incomedanexpense/Note.js
@@ -1,44 +1,96 @@
 const {Income, Expense} = require('./IncomeExpense')
+const fs = require('fs');
 
 class Note{
-    constructor(list){
-        this.list = list || [];
-    }
-    addIncome(name, total){
-        let income = new Income(name, total, 'income');
-        this.list.push(income);
-    }
-    addExpense(name, total){
-        let expense = new Expense(name, total, 'expense');
-        this.list.push(expense);
-    }
-    listIncome(){
+    // constructor(list){
+    //     this.list = list || [];
+    // }
+    // addIncome(name, total){
+    //     let income = new Income(name, total, 'income');
+    //     this.list.push(income);
+    // }
+    // addExpense(name, total){
+    //     let expense = new Expense(name, total, 'expense');
+    //     this.list.push(expense);
+    // }
+    // listIncome(){
+    //     console.log("List Income: ");
+    //     this.list.forEach(inc => {
+    //         if(inc.category === 'income'){
+    //             console.log(`${inc.name}, Rp. ${inc.total}`);
+    //         }
+    //     })
+    // }
+    // listExpense(){
+    //     console.log("List Expense: ");
+    //     this.list.forEach(inc => {
+    //         if(inc.category === 'expense'){
+    //             console.log(`${inc.name}, Rp. ${inc.total}`);
+    //         }
+    //     })
+    // }
+    // balance(){
+    //     let totalIncome = 0;
+    //     let totalExpense = 0;
+    //     this.list.forEach(el =>{
+    //         if(el.category === 'income'){
+    //             totalIncome += el.total;
+    //         }else if(el.category === 'expense'){
+    //             totalExpense += el.total;
+    //         }
+    //     }) 
+
+    //     let total = totalIncome - totalExpense;
+    //     if(total === 0){
+    //         console.log("Status: Balanced!");
+    //     }else if(total < 0){
+    //         console.log("Status : Minus!");
+    //     }else if(total > 0){
+    //         console.log("Status : Plus!");
+    //     }
+    // }
+    static listIncome(){
+        let temp = JSON.parse(fs.readFileSync('./dataInEx.json', 'utf8'));
+        let incomes = temp.incomes;
         console.log("List Income: ");
-        this.list.forEach(inc => {
-            if(inc.category === 'income'){
-                console.log(`${inc.name}, Rp. ${inc.total}`);
-            }
+        incomes.forEach(income => {
+            const{name, total} = income;
+            console.log(`${name}, Rp. ${total}`);
         })
     }
-    listExpense(){
+    static listExpense(){
+        let temp = JSON.parse(fs.readFileSync('./dataInEx.json', 'utf8'));
+        let expenses = temp.expenses;
         console.log("List Expense: ");
-        this.list.forEach(inc => {
-            if(inc.category === 'expense'){
-                console.log(`${inc.name}, Rp. ${inc.total}`);
-            }
+        expenses.forEach(expense => {
+            const{name, total} = expense;
+            console.log(`${name}, Rp. ${total}`);
         })
     }
-    balance(){
+    static addIncome(name, total){
+        let temp = JSON.parse(fs.readFileSync('./dataInEx.json', 'utf8'));
+        let income = new Income(name, total, 'income');
+        temp.incomes.push(income);
+        // console.log(temp);
+        this.save(temp);
+    }
+    static addExpense(name, total){
+        let temp = JSON.parse(fs.readFileSync('./dataInEx.json', 'utf8'));
+        let expense = new Expense(name, total, 'expense');
+        temp.expenses.push(expense);
+        // console.log(temp);
+        this.save(temp);
+    }
+    static balance(){
+        let temp = JSON.parse(fs.readFileSync('./dataInEx.json', 'utf8'));
         let totalIncome = 0;
         let totalExpense = 0;
-        this.list.forEach(el =>{
-            if(el.category === 'income'){
-                totalIncome += el.total;
-            }else if(el.category === 'expense'){
-                totalExpense += el.total;
-            }
-        }) 
-
+        temp.incomes.forEach(income => {
+            totalIncome += income.total;
+        })
+        temp.expenses.forEach(expense => {
+            totalExpense += expense.total;
+        })
         let total = totalIncome - totalExpense;
         if(total === 0){
             console.log("Status: Balanced!");
@@ -47,6 +99,9 @@ class Note{
         }else if(total > 0){
             console.log("Status : Plus!");
         }
+    }
+    static save(data){
+        fs.writeFileSync('./dataInEx.json',JSON.stringify(data, null, 3));
     }
 }
 
--- incomedanexpense/index.js
+++ incomedanexpense/index.js
@@ -1,8 +1,13 @@
 const Note = require('./Note'); //import
 
-let note = new Note();
-note.addIncome("Gaji bulanan", 5000);
-note.addExpense("Bayar air", 5000);
-note.listIncome();
-note.listExpense();
-note.balance();
+// let note = new Note();
+// note.addIncome("Gaji bulanan", 5000);
+// note.addExpense("Bayar air", 5000);
+// note.listExpense();
+// note.balance();
+
+// Note.listIncome();
+// Note.listExpense(); 
+// Note.addIncome("Uang cair", 3000);
+// Note.addExpense("Bayar utang", 3000);
+Note.balance();
