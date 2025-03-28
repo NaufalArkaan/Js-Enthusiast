4a6c01233a902a290633e0ef1a3aa509fa1c61ce
NaufalArkaan <naufalarkaan25@gmail.com>
2025-03-28 20:55:58 +0700

    part II web kuitansi

---
+++ web-kuitansi/js/script.js
@@ -0,0 +1,25 @@
+let BtnSubmit = document.getElementById('btnSubmit');
+
+let carts = []
+
+function submitHandler(){
+    let id;
+    if(carts.length === 0){
+        id = 1;
+    }else{
+        id = carts[carts.length -1].id + 1;
+    }
+    let barangValue = document.getElementById('barang').value;
+    let hargaValue = document.getElementById('harga').value;
+    let tempObj = {
+        id,
+        nama : barangValue,
+        harga : +hargaValue
+    }
+    carts.push(tempObj);
+    console.log(carts);
+}
+
+BtnSubmit.addEventListener('click', submitHandler);
+
+console.log(BtnSubmit);
